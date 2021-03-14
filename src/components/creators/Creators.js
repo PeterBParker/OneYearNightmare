81414e1cf6b2e38606cc10f2a666d20553cef81e
Nathan Harris <harrihaven2@gmail.com>
2021-03-13 21:32:21 -0500

    updated bio content

--- src/components/creators/Creators.js
+++ src/components/creators/Creators.js
@@ -4,11 +4,11 @@ export default function Creators() {
     return(
         <div className="aboutUsPage">
             <h1>
-                Nathan Harris is chillin'
+                Rayell Creators
             </h1>
-            <h1>
-                Morghan Harris is awesome
-            </h1>
+            <p>
+                We are a creative couple that LOVES stories, imagery, and anything novel. Morghan Harris is the visual artist of the two and did the design of the website from scratch and created all the wonderful assets you see (including the graphic novel pages).
+                Nathan Harris is a big fan of words, and as such, he fill in as writer of One Year Nightmare and developer of this website. Check out the other stuff we create at our social media or hop on our discord and say hi!</p>
         </div>
     );
 }
