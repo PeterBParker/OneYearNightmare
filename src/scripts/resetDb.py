8ddcac9bc02ddfe9b591293d5688264773684571
Nathan Harris <harrihaven2@gmail.com>
2021-04-01 20:40:28 -0400

    added a databse wipe for when we start prod

---
+++ src/scripts/resetDb.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+_DATA_FILENAME = "../api/data/pagesData.json"
+
+if __name__ == "__main__":
+    print("Are you sure you want to reset the database? This action is irreversible.\n")
+    answer = input("absolutely / no > ")
+    if answer = "absolutely":
+        newBook = {
+            "pageCount": 0,
+            "seasonCount": 0,
+            "seasons": []
+        }
+        with open(_DATA_FILENAME, 'w', encoding='utf-8') as f:
+            json.dump(newBook, fensure_ascii=False, indent=4)
+            json.truncate()
