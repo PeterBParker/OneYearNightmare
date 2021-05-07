107c38dac3678b0ad248e704779c8b42c35fcee7
Nathan Harris <harrihaven2@gmail.com>
2021-05-07 18:38:41 -0400

    implemented hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,25 @@
-import { Link, useParams } from 'react-router-dom';
-import { COMIC_VIEWER_DEFAULT_PATH } from './Main';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/Website Assets - Phase 1/SVG/LOGO-header.svg';
 import menuIcon from '../assets/Website Assets - Phase 1/SVG/MOBILE-sandwhich-menu.svg';
 import background from '../assets/Website Assets - Phase 1/SVG/MOBILE-header.svg'
 import ComicPageAPI from '../api/ComicPageAPI';
 import pageData from '../api/data/pagesData.json';
+import MobileMenu from './generic/menus/MobileMenu';
 
 export default function Header() {
+    const [showMenu, setShowMenu] = useState(false);
     let volNum = ComicPageAPI.getSeasonNum(pageData.pageCount);
+    function toggleMenu() {
+        if (showMenu == true) {
+            setShowMenu(false);
+        } else {
+            setShowMenu(true);
+        }
+    }
     return (
         <div>
-            <div className="navbar">
-                    <div className="comicViewerNavLink">
-                        <Link to={COMIC_VIEWER_DEFAULT_PATH}>read</Link>
-                    </div>
-                    <div className="creatorsNavLink">
-                        <Link to='/creatives'>about us</Link>
-                    </div>
-                    <div className="supportNavLink">
-                        <Link to='/support'>support us</Link>
-                    </div>
-            </div>
+            <MobileMenu showMenu={showMenu} onMenuChange={toggleMenu}/> 
             <div className="headerGrid w-full pt-3">
                 <div className="volDisplayMobile text-mocha-dark font-medium text-left ml-6 self-start">
                     <div className="volTextContainer flex flex-row w-full pt-3">
@@ -30,8 +29,8 @@ export default function Header() {
                         <div className="volNumContainer ml-2">
                             <svg width="32px" height="32px">
                                 <g>
-                                    <circle cx="14" cy="14"  r="13" stroke-width="2" stroke="#998f7e" fill-opacity="0%"></circle>
-                                    <text x="14" y="14" fill="#998f7e" className="headerVolNum" text-anchor="middle" alignment-baseline="central">{volNum}</text>
+                                    <circle cx="14" cy="14"  r="13" strokeWidth="2" stroke="#998f7e" fillOpacity="0%"></circle>
+                                    <text x="14" y="14" fill="#998f7e" className="headerVolNum" textAnchor="middle" alignmentBaseline="central">{volNum}</text>
                                 </g>
                             </svg>
                         </div>
@@ -43,7 +42,7 @@ export default function Header() {
                     <img src={logo} width="170" />
                 </div>
                 <div className="navMenuMobile ml-auto mr-6">
-                    <img src={menuIcon} width={42} />
+                    <button onClick={toggleMenu}><img src={menuIcon} width={42} /></button>
                 </div>
                 <div className="publishDaysMobile text-mocha-dark self-end bg-cream-dark font-medium h-full">
                     <p>mon | thur</p>
---
+++ src/components/generic/menus/MobileMenu.js
@@ -0,0 +1,42 @@
+import footerImage from '../../../assets/Website Assets - Phase 1/SVG/MOBILE-menu-image.svg';
+import exitButton from '../../../assets/Website Assets - Phase 1/SVG/MOBILE-x.svg';
+import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { COMIC_VIEWER_DEFAULT_PATH } from '../../Main';
+
+export default function MobileMenu(props) {
+    let mobileMenu = useRef(null);
+
+    if (props.showMenu && mobileMenu.current) {
+        mobileMenu.current.style.right = "0";
+    } else if (!props.showMenu && mobileMenu.current) {
+        mobileMenu.current.style.right = "-100%";
+    }
+    return (
+        <div className='mobileMenuOverlay' ref={mobileMenu}>
+            <div className="mobileMenuContainer bg-grey-light">
+                <div className="mobileMenuExitContainer grid justify-end">
+                    <div className="mr-3">
+                        <button onClick={props.onMenuChange} className="mobileMenuExitButton"><img src={exitButton} width={32} /></button>
+                    </div>
+                </div>
+                <div className="mobileMenuLinks text-green font-montserrat text-3xl">
+                    <div className="mobileMenuLinksSpacer grid grid-rows-3 grid-cols-1">
+                        <div className="comicViewerNavLink">
+                            <Link to={COMIC_VIEWER_DEFAULT_PATH} onClick={props.onMenuChange}>read</Link>
+                        </div>
+                        <div className="creatorsNavLink">
+                            <Link to='/creatives' onClick={props.onMenuChange}>about us</Link>
+                        </div>
+                        <div className="supportNavLink">
+                            <Link to='/support' onClick={props.onMenuChange}>support us</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="mobileMenuFooterImage w-full">
+                    <img src={footerImage} />
+                </div>
+            </div>
+        </div>
+    );
+}
