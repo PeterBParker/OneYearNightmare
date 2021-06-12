a1c57bc0ec3097c9123d9fef24e9fa77559d3c3d
Nathan Harris <harrihaven2@gmail.com>
2021-06-11 21:48:44 -0400

    header content deskt with header backgroudn image

--- src/components/header/DesktopHeader.jsx
+++ src/components/header/DesktopHeader.jsx
@@ -8,35 +8,37 @@ export default function DesktopHeader(props) {
 
     return (
         <div>
-            <div className={`headerGrid w-full pt-3 ${props.defaultBg ? "bg-cream-light": "bg-white" }`}>
-                <div className="volDisplayMobile text-mocha-dark font-medium text-left ml-6 self-start">
-                    <div className="volTextContainer flex flex-row w-full pt-3">
-                        <div className="volText headerVolNum">
-                            vol
-                        </div>
-                        <div className="volNumContainer ml-2">
-                            <svg width="32px" height="32px">
-                                <g>
-                                    <circle cx="14" cy="14"  r="13" strokeWidth="2" stroke="#998f7e" fillOpacity="0%"></circle>
-                                    <text x="14" y="14" fill="#998f7e" className="headerVolNum" textAnchor="middle" alignmentBaseline="central">{volNum}</text>
-                                </g>
-                            </svg>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                <div className="headerLogoMobile justify-self-center self-start">
-                    <img src={logo} width="170" />
-                </div>
-                <div className={`publishDaysMobile text-mocha-dark self-end font-medium h-full ${props.defaultBg ? "bg-white" : "bg-cream-dark"}`}>
-                    <p>mon | thur</p>
-                </div>
-                <div className="farmHeaderBackground grid pt-10">
+            <div className={`headerGridDesktop items-center w-full pt-3 ${props.defaultBg ? "bg-cream-light" : "bg-white"}`}>
+            <div className="farmHeaderBackground pt-10">
                     <div className="self-end">
                         <MobileHeaderBackground color={`${props.defaultBg ? "#fff" : "#e0dcd1"}`} />
                     </div>
-                    
+
+                </div>
+                <div className="headerContentDesktop items-center">
+                    <div className={`publishDaysDesktop text-mocha-dark self-center font-medium justify-self-end mr-3 text-xl`}>
+                        <p>mon | thur</p>
+                    </div>
+                    <div className="headerLogoDesktop justify-self-center self-start p-7">
+                        <img className="headerLogoImageDesktop" src={logo} width="300px" height="auto"/>
+                    </div>
+                    <div className="volDisplayDesktop text-mocha-dark font-medium text-left ml-6 self-center justify-self-start">
+                        <div className="volTextContainer flex flex-row w-full">
+                            <div className="volText headerVolNumDesktop text-xl">
+                                volume
+                        </div>
+                            <div className="volNumContainer ml-2 ">
+                                <svg width="32px" height="32px">
+                                    <g>
+                                        <circle cx="14" cy="14" r="13" strokeWidth="2" stroke="#998f7e" fillOpacity="0%"></circle>
+                                        <text x="14" y="14" fill="#998f7e" className="headerVolNum" textAnchor="middle" alignmentBaseline="central">{volNum}</text>
+                                    </g>
+                                </svg>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
