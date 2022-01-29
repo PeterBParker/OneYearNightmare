6be9eac2607420490e145ca5f403b0cf2d3bdc3f
Nathan Harris <harrihaven2@gmail.com>
2022-01-28 20:05:23 -0600

    graceful comic image loading start

--- src/components/comics/ComicViewerCards/DesktopPageView.jsx
+++ src/components/comics/ComicViewerCards/DesktopPageView.jsx
@@ -1,9 +1,11 @@
 import VerticalShare from '../VerticalShare';
+import GracefulImage from '../GracefulImage';
 
 export default function DesktopPageView(props) {
     return (
         <div className={"desktopComicPageContainer"}>
-            <img ref={props.topOfPageRef} src={props.pageImageUrl} alt="test" className="comicPage"/>
+            <GracefulImage src={props.pageImageUrl} className="comicPage" reference={props.topOfPageRef} keyVal="desktopComicPageImage"/>
+            {/*<img  ref={props.topOfPageRef} src={props.pageImageUrl} alt="test" className="comicPage"/>*/}
             <div className="comicViewerVertShare ml-4">
                 <VerticalShare sharePageUrl={props.sharePageUrl} title={props.title} shareImageUrl={props.pageImageUrl}/>
             </div>
