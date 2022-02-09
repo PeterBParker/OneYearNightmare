30e046fbca4a830583cda95c10893cc934969110
Nathan <harrihaven2@gmail.com>
2022-02-08 19:30:56 -0600

    added icon generation and split the icon classes

--- src/scripts/IconifyPages.py
+++ src/scripts/IconifyPages.py
@@ -1,21 +1,20 @@
 import addPage as api
 import os
 from pathlib import Path
-from PIL import Image, UnidentifiedImageError
+from IconMaker import IconMaker
 
 
 class IconifyPagesController:
-    def __init__(self, icon_suffix="-Icon.png", icon_dir=Path("icons/"), icon_dims=(200,200)):
+    def __init__(
+        self, icon_suffix="-Icon.png", icon_dir=Path("icons/"), icon_dims=(200, 200)
+    ):
         self.icon_suffix = icon_suffix
         self.icon_dir = icon_dir
         self.icon_dims = icon_dims
 
-    def getIconName(self, base_page_path):
-        return os.path.splitext(os.path.basename(base_page_path))[0] + self.icon_suffix
-
     def iconifyExistingPage(self, page_file_path, icon_dir):
-        """ Saves a copy of an image as an icon. 
-        
+        """Saves a copy of an image as an icon.
+
         :param page_file_path(pathlib.Path): An absolute filepath to an image
         :param icon_dir(pathlib.Path): An absolute filepath to the output dir
 
@@ -25,7 +24,7 @@ class IconifyPagesController:
         icon_filename = self.getIconName(page_file_path)
         icon_path = icon_dir / icon_filename
         iconer = IconMaker()
-        iconer.iconify_file(self.icon_dims, page_file_path, icon_path)
+        iconer.iconify_file(self.icon_dims, page_file_path, iconer.getIconName())
         return icon_path
 
     def iconifyAllExistingPages(self):
@@ -40,22 +39,6 @@ class IconifyPagesController:
                     if os.path.basename(page) == "icons":
                         continue
                     self.iconifyExistingPage(page, icon_dir)
-
-    def addAnIconPathToDb(self, db_filename, icon_path, page_id):
-        """ Adds the path of an icon to the db json file.
-        
-        :param db_filename(pathlib.Path): Absolute path to db file
-        :param icon_path(pathlib.Path): Absolute path to icon
-        :param page_id(int): Unique id of page
-        """
-        data = api.getData(db_filename, "r")
-        chapter_dir = icon_path.parent.parent
-        season_dir = chapter_dir.parent
-        season = next(season for season in data["seasons"] if season["foldername"] == season_dir.name)
-        chapter = next(chapter for chapter in season["chapters"] if chapter["foldername"] == chapter_dir.name)
-        page = next(page for page in chapter["pages"] if page["id"] == page_id)
-        page["icon"] == self.icon_dir / icon_path.name
-        api.writeToFile(data, db_filename)
 
     def addIconPathsToDb(self, db_filename):
         data = api.getData(db_filename, "r")
@@ -72,40 +55,6 @@ class IconifyPagesController:
     def generateAndAddIcons(self, db_filename):
         self.iconifyAllExistingPages()
         self.addIconPathsToDb(db_filename)
-
-    def generateAndAddAIcon(self, db_filename):
-        self.ic
-
-
-class IconMaker:
-    def iconify_file(self, dimensions, image_path, output_path):
-        """Takes an image and creates a customizable icon
-
-        :param dimensions(tuple(int,int)): The maximum desired height in pixels
-        :param image_path(str): The file path for the original image
-        :param output_path(str): The file path for the output icon
-        """
-        if self.validate_file(image_path):
-            im = Image.open(image_path)
-            im.thumbnail(dimensions)
-            im.save(output_path)
-
-    @staticmethod
-    def validate_file(file_path):
-        """Filters out the files that aren't images
-
-        :param file_path(str): The file path of a suspected image
-
-        :returns: Whether the file is an image or not
-        :rtype: boolean
-        """
-        isImage = False
-        try:
-            if Image.open(file_path):
-                isImage = True
-        except (UnidentifiedImageError, ValueError):
-            pass
-        return isImage
 
 
 if __name__ == "__main__":
--- src/scripts/addPage.py
+++ src/scripts/addPage.py
@@ -3,7 +3,7 @@ import json
 import ntpath
 import os
 
-from IconifyPages import IconifyPagesController
+from IconMaker import IconMaker
 from pathlib import Path
 from shutil import copyfile
 from num2words import num2words
@@ -15,7 +15,9 @@ _DATA_FILENAME = "../api/data/pagesData.json"
 _USERS_FILENAME = "../api/data/users.json"
 _WORKING_DATA_FILENAME = "../api/data/pagesData-temp.json"
 _BACKUP_FILENAME = "../api/data/pagesData-backup.json"
-_DIR_PREFIX = '../../public/MnMPages/'
+_DIR_PREFIX = "../../public/MnMPages/"
+_ICON_REL_DIR = "icons"  # The directory to save icons in relative to the chapter dir
+
 
 def addChapter(seasonName, chapterName, folderName, numOfPages, id, pages):
     chapterObj = {
@@ -23,18 +25,61 @@ def addChapter(seasonName, chapterName, folderName, numOfPages, id, pages):
         "folderName": folderName,
         "numOfPages": numOfPages,
         "id": id,
-        "pages": pages
+        "pages": pages,
     }
     writeChapterAppend(chapterObj, seasonName)
     season = getSeason(seasonName)
     makeDirectory(_DIR_PREFIX + season["folderName"] + "/" + folderName)
 
+
 def addFirstChapter(seasonName):
-    addChapter(seasonName=seasonName, chapterName="Chapter One", folderName="chapter1", numOfPages=0, id=1, pages=[])
+    addChapter(
+        seasonName=seasonName,
+        chapterName="Chapter One",
+        folderName="chapter1",
+        numOfPages=0,
+        id=1,
+        pages=[],
+    )
+
+
+def generateThumbnail(image_path):
+    """Generates a thumbnail and saves it to the icon dir.
+
+    :param image_path(pathlib.Path): path to target image to iconify
+    """
+    iconer = IconMaker()
+    icon_path = image_path.parent / _ICON_REL_DIR / iconer.getIconName(image_path)
+    iconer.iconify_file((200, 200), image_path, icon_path)
+    return icon_path
+
+
+def addAnIconPathToDb(db_filename, icon_path, page_id):
+    """Adds the path of an icon to the db json file.
+
+    :param db_filename(pathlib.Path): Absolute path to db file
+    :param icon_path(pathlib.Path): Absolute path to icon
+    :param page_id(int): Unique id of page
+    """
+    data = getData(db_filename, "r")
+    chapter_dir = icon_path.parent.parent
+    season_dir = chapter_dir.parent
+    season = next(
+        season for season in data["seasons"] if season["folderName"] == season_dir.name
+    )
+    chapter = next(
+        chapter
+        for chapter in season["chapters"]
+        if chapter["folderName"] == chapter_dir.name
+    )
+    page = next(page for page in chapter["pages"] if page["id"] == page_id)
+    page["icon"] == str(Path(_ICON_REL_DIR) / icon_path.name)
+    writeToFile(data, db_filename)
+
 
 def addNewPage(seasonName, chapterName, title, message, filepath, user):
     # time, id, and pageNum are generated off of existing values
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     chapter = getChapterInSeason(seasonName, chapterName)
     pageNum = chapter["numOfPages"] + 1
     identifier = data["pageCount"] + 1
@@ -43,15 +88,15 @@ def addNewPage(seasonName, chapterName, title, message, filepath, user):
     # Copy image and truncate filename
     filename = pathLeaf(filepath)
     season = getSeason(seasonName)
-    destDir = Path( _DIR_PREFIX + season["folderName"] + "/" + chapter["folderName"] + "/")
+    destDir = Path(
+        _DIR_PREFIX + season["folderName"] + "/" + chapter["folderName"] + "/"
+    )
     destFilepath = destDir / filename
     copyfile(filepath, destFilepath)
 
     # Generate icon and add it to db
-    iconer = IconifyPagesController()
-    icon_dir = destDir / iconer.icon_dir
-    icon_path = iconer.iconifyExistingPage(destFilepath, icon_dir)
-    iconer.addAnIconPathToDb(_WORKING_DATA_FILENAME, icon_path, identifier)
+
+    icon_path = generateThumbnail(destFilepath)
 
     newPage = {
         "pageNum": pageNum,
@@ -61,6 +106,7 @@ def addNewPage(seasonName, chapterName, title, message, filepath, user):
         "datetime": time,
         "id": identifier,
         "user": user,
+        "icon": str(Path(_ICON_REL_DIR) / icon_path.name),
     }
 
     writeIncrementChapterPageCount(season["id"], chapter["id"])
@@ -72,19 +118,22 @@ def addNewPage(seasonName, chapterName, title, message, filepath, user):
 
     return True
 
+
 def appendSeason():
     newSeasonName = enterSeasonName()
     seasonObj = {}
     seasonObj["seasonName"] = newSeasonName
-    if not os.path.exists('../../public/OneYearNightmarePages/'+newSeasonName.lower()):
-        os.makedirs(_DIR_PREFIX+newSeasonName.lower())
+    if not os.path.exists(
+        "../../public/OneYearNightmarePages/" + newSeasonName.lower()
+    ):
+        os.makedirs(_DIR_PREFIX + newSeasonName.lower())
     else:
         raise Exception("Season directory already exists for specified season.")
     seasonObj["folderName"] = newSeasonName.lower()
     seasonObj["numOfPages"] = 0
     seasonObj["maxDisplayPage"] = 0
     seasonObj["numOfChapters"] = 0
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     seasonObj["id"] = data["seasonCount"] + 1
     seasonObj["chapters"] = []
 
@@ -95,10 +144,11 @@ def appendSeason():
 
     return newSeasonName
 
+
 def createNewChapter(seasonName):
     seasonIndex = getSeasonIndex(seasonName)
     season = getSeason(seasonName)
-    
+
     chapterNum = season["numOfChapters"] + 1
     chapterName = "Chapter " + num2words(chapterNum).capitalize()
 
@@ -106,45 +156,50 @@ def createNewChapter(seasonName):
     folderName = "chapter" + str(chapterNum)
     print("This is the folderName: ", folderName)
     input("Pause")
-    makeDirectory(_DIR_PREFIX+season["folderName"]+'/'+folderName)
-    
+    makeDirectory(_DIR_PREFIX + season["folderName"] + "/" + folderName)
+
     addChapter(seasonName, chapterName, folderName, 0, chapterNum, [])
     return chapterName
-    
-    
+
+
 def doesSeasonExist(seasonName):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
-    for season in data['seasons']:
-            if seasonName.lower() == season["seasonName"].lower():
-                return True
+    data = getData(_WORKING_DATA_FILENAME, "r")
+    for season in data["seasons"]:
+        if seasonName.lower() == season["seasonName"].lower():
+            return True
     return False
 
+
 def getData(filename, r_or_w):
-    with open(filename, r_or_w, encoding='utf-8') as dataFile:
+    with open(filename, r_or_w, encoding="utf-8") as dataFile:
         data = json.load(dataFile)
     return data
 
+
 def getChapterInSeason(seasonName, chapterName):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     seasonObj = getSeason(seasonName)
     for chapter in seasonObj["chapters"]:
-        if(chapter["chapterName"] == chapterName):
+        if chapter["chapterName"] == chapterName:
             return chapter
     return None
 
+
 def getLastChapterNameInSeason(seasonName):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     seasonObj = getSeason(seasonName)
     chapterIndex = seasonObj["numOfChapters"] - 1
     lastChapterObj = seasonObj["chapters"][chapterIndex]
     return lastChapterObj["chapterName"]
 
+
 def getLatestSeason():
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     if len(data["seasons"]) == 0:
         return None
     latestSeason = max(data["seasons"], key=lambda x: x["id"])
     return latestSeason
+
 
 def getLatestSeasonName():
     latestSeason = getLatestSeason()
@@ -152,22 +207,26 @@ def getLatestSeasonName():
         return None
     return latestSeason["seasonName"]
 
+
 def getSeason(seasonName):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     for season in data["seasons"]:
-        if(seasonName == season["seasonName"]):
+        if seasonName == season["seasonName"]:
             return season
     raise Exception("Season not found")
 
+
 def getSeasonIndex(seasonName):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     for season in data["seasons"]:
-        if(seasonName == season["seasonName"]):
-            return season["id"] -1
+        if seasonName == season["seasonName"]:
+            return season["id"] - 1
     raise Exception("Season not found")
+
 
 def getTargetChapter(seasonName):
     return enterNewOrExistingChapter(seasonName)
+
 
 def getTargetSeason():
     targetSeason = None
@@ -177,16 +236,21 @@ def getTargetSeason():
             print("\nNo seasons found. Please create a season.\n")
             targetSeason = appendSeason()
             continue
-        print("\nPlease select an option:\n1) Append to " + latestSeasonName + "\n2) Add a new season\n")
+        print(
+            "\nPlease select an option:\n1) Append to "
+            + latestSeasonName
+            + "\n2) Add a new season\n"
+        )
         selection = input("> ")
-        if (selection == "1"):
+        if selection == "1":
             targetSeason = latestSeasonName
             break
         elif selection == "2":
             targetSeason = appendSeason()
         else:
             print("\nI don't recognize that option. Try again.\n")
-    return (targetSeason)
+    return targetSeason
+
 
 def getUser():
     userData = getData(_USERS_FILENAME, "r")
@@ -211,35 +275,44 @@ def getUser():
             print("That id doesn't match any known user :(")
             continue
 
+
 def enterSeasonName():
     decidingOnSeasonName = True
     newSeasonName = None
-    while(decidingOnSeasonName):
+    while decidingOnSeasonName:
         print("\nAdding a Season! Please enter the name of the new season:\n")
         newSeasonName = input("Enter New Season Name > ")
-        if(doesSeasonExist(newSeasonName)):
+        if doesSeasonExist(newSeasonName):
             print("Season already exists! Please add a different season.\n")
             continue
-        print("\nConfirm you want to add the season, " + newSeasonName +'\n')
+        print("\nConfirm you want to add the season, " + newSeasonName + "\n")
         confirmation = input("y/n > ")
         if confirmation == "y":
             decidingOnSeasonName = False
     return newSeasonName
 
+
 def enterNewOrExistingChapter(seasonName):
     while True:
         lastChapterName = getLastChapterNameInSeason(seasonName)
-        print("\nPlease select an option:\n1) Append to " + seasonName + ", " + lastChapterName + "\n2) Create a new chapter?\n")
+        print(
+            "\nPlease select an option:\n1) Append to "
+            + seasonName
+            + ", "
+            + lastChapterName
+            + "\n2) Create a new chapter?\n"
+        )
         appendOrCreate = input("> ")
-        if (appendOrCreate == "1"):
+        if appendOrCreate == "1":
             break
-        elif (appendOrCreate == "2"):
+        elif appendOrCreate == "2":
             lastChapterName = createNewChapter(seasonName)
             break
     return lastChapterName
 
+
 def enterPageData(seasonName, chapterName):
-    #user needs to input filename, message, and title
+    # user needs to input filename, message, and title
     title = None
     message = None
     filepath = None
@@ -251,19 +324,34 @@ def enterPageData(seasonName, chapterName):
         while True:
             print("\nPlease type the absolute filepath for this page:\n")
             filepath = input("> ")
-            if(os.path.exists(filepath)):
+            if os.path.exists(filepath):
                 break
             else:
-                print("\nThat filepath cannot be found. Check your file extensions, path, and try again.")
-        print("\nConfirm you want to add a page with these details:\n\
-                            - Append to the end of " + seasonName + ", " + chapterName + "\n\
-                            - Title: " + title + "\n\
-                            - Filepath: " + filepath + "\n\
-                            - Message: " + message + "\n")
+                print(
+                    "\nThat filepath cannot be found. Check your file extensions, path, and try again."
+                )
+        print(
+            "\nConfirm you want to add a page with these details:\n\
+                            - Append to the end of "
+            + seasonName
+            + ", "
+            + chapterName
+            + "\n\
+                            - Title: "
+            + title
+            + "\n\
+                            - Filepath: "
+            + filepath
+            + "\n\
+                            - Message: "
+            + message
+            + "\n"
+        )
         redo = input("y/n > ")
-        if(redo == "y"):
+        if redo == "y":
             break
     return title, message, filepath
+
 
 def makeDirectory(dirpath):
     if not os.path.exists(dirpath):
@@ -273,9 +361,11 @@ def makeDirectory(dirpath):
         if len(os.listdir(dirpath)) != 0:
             raise Exception("Directory already exists")
 
+
 def pathLeaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
+
 
 def safeCopyFile(filename, dest):
     if os.path.exists(filename):
@@ -284,63 +374,74 @@ def safeCopyFile(filename, dest):
     else:
         raise FileNotFoundError("No Data File Found")
 
+
 def writeChapterAppend(chapterObj, seasonName):
 
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
 
     seasonObj = getSeason(seasonName)
     seasonObj["chapters"].append(chapterObj)
     seasonObj["numOfChapters"] += 1
-    seasonIndex = seasonObj["id"] -1
+    seasonIndex = seasonObj["id"] - 1
     # Replace the proper season object with our newly created one.
     data["seasons"][seasonIndex] = seasonObj
 
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeIncrementBookPageCount():
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     data["pageCount"] += 1
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeIncrementMaxDisplayPageCount():
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     data["maxDisplayPage"] += 1
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeIncrementSeasonCount():
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     data["seasonCount"] += 1
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeIncrementChapterPageCount(seasonId, chapterId):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
-    data["seasons"][seasonId-1]["chapters"][chapterId-1]["numOfPages"] += 1
+    data = getData(_WORKING_DATA_FILENAME, "r")
+    data["seasons"][seasonId - 1]["chapters"][chapterId - 1]["numOfPages"] += 1
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeNewSeasonOverwrite(seasonObj, seasonIndex):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     data["seasons"][seasonIndex] = seasonObj
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeNewSeasonAppend(seasonObj):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
+    data = getData(_WORKING_DATA_FILENAME, "r")
     data["seasons"].append(seasonObj)
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writePageAppend(seasonId, chapterId, newPage):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
-    data["seasons"][seasonId-1]["chapters"][chapterId-1]["pages"].append(newPage)
+    data = getData(_WORKING_DATA_FILENAME, "r")
+    data["seasons"][seasonId - 1]["chapters"][chapterId - 1]["pages"].append(newPage)
     writeToFile(data, _WORKING_DATA_FILENAME)
+
 
 def writeIncrementSeasonPageCount(seasonId):
-    data = getData(_WORKING_DATA_FILENAME, 'r')
-    data["seasons"][seasonId-1]["numOfPages"] += 1
+    data = getData(_WORKING_DATA_FILENAME, "r")
+    data["seasons"][seasonId - 1]["numOfPages"] += 1
     writeToFile(data, _WORKING_DATA_FILENAME)
 
+
 def writeToFile(data, filename):
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
         f.truncate()
+
 
 if __name__ == "__main__":
     try:
@@ -350,10 +451,12 @@ if __name__ == "__main__":
         # Make a copy of the data file to make intermediate changes to
         workingDataFileName = safeCopyFile(_DATA_FILENAME, _WORKING_DATA_FILENAME)
 
-        print("Welcome to the Page Appender! I will append a page to the end of a specifed chapter.\n")
+        print(
+            "Welcome to the Page Appender! I will append a page to the end of a specifed chapter.\n"
+        )
 
         user = getUser()
-        
+
         # Get Season Name to add the page to
         seasonName = getTargetSeason()
 
@@ -365,11 +468,11 @@ if __name__ == "__main__":
 
         print("Adding page...")
         isSuccess = addNewPage(seasonName, chapterName, title, message, filepath, user)
-        if(isSuccess):
+        if isSuccess:
             print("Page successfully added!")
-        
+
         # Overwrite data file with temp file
         copyfile(_WORKING_DATA_FILENAME, _DATA_FILENAME)
-        print("Exiting...")        
+        print("Exiting...")
     finally:
         os.remove(workingDataFileName)
