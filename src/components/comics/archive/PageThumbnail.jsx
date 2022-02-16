d58400cf2126ed744c0be336139eb03fda5468b2
Nathan Harris <harrihaven2@gmail.com>
2022-02-15 19:38:32 -0600

    added styling for archive page thumbnails

--- src/components/comics/archive/PageThumbnail.jsx
+++ src/components/comics/archive/PageThumbnail.jsx
@@ -8,21 +8,17 @@ export default function PageThumbnail(props) {
 
 
     return(
-        <Link to={`${COMIC_VIEWER_PATH}/${props.pageId}`} className="m-3 pageThumbnail relative" onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
-            <img src={props.imageSrc} loading="lazy" width={200} height={200} alt={props.altText} className={` block w-100 background-image mx-auto transition-all filter drop-shadow ${hovering ? "blur-sm grayscale brightness-90": null}`}/>
-            {hovering?
-                <div className="absolute top-10 left-0 text-eggshell w-full">
-                    <div className="font-medium">
+        <Link to={`${COMIC_VIEWER_PATH}/${props.pageId}`} className="m-3 py-2 pageThumbnail relative" onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
+            <div className={`rounded filter bg-white grow pt-2 px-2${hovering? 'drop-shadow-md': 'drop-shadow'}`}>
+                <img src={props.imageSrc} loading="lazy" width={200} height={200} alt={props.altText} className="mx-auto"/>
+                <div className="text-grey-dark text-center archivePageThumbnailBanner">
+                    <div className="font-medium text-lg px-2">
                         Page #{props.pageId}
                     </div>
-                    <br/>
-                    <div className="text-lg text-left px-4">
-                        {props.altText}
-                    </div>
-
                 </div>
-            : 
-                null}
+            </div>
+
+            
         </Link>
     )
 }
