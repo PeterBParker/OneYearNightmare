96212db035f22205a292c4220b3d10ae32b8542b
Nathan Harris <harrihaven2@gmail.com>
2022-02-17 21:28:44 -0600

    implemented accordion animation on archive page

--- src/components/comics/archive/ChapterBanner.jsx
+++ src/components/comics/archive/ChapterBanner.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BASE_PATH } from "../../Main";
 import PageThumbnail from "./PageThumbnail";
 
@@ -7,19 +7,35 @@ export default function ChapterBanner(props) {
     const [isActive, setIsActive] = useState(false);
     // Not using hover because I only want the effect triggered once per mouse entry
     const [isMouseOver, setIsMouseOver] = useState(false); 
-
     let pagesDisplay = BuildChapterPagesDisplay(props.chapter, props.chapterPath);
 
 
+    useEffect(() => {
+        const thisAccordion = document.getElementById(props.bannerId);
+        const content = thisAccordion.querySelector(".accordion__content");
+        if(isActive) {
+            // Open the page thumbnail accordion
+            thisAccordion.classList.add("accordion__active");
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            thisAccordion.classList.remove("accordion__active");
+            content.style.maxHeight = null;
+        }
+    }, [isActive])
+
+
+
+
     return(
-        <div className="my-4 mx-6 font-header text-3xl text-center cursor-pointer">
-            <div className={`py-4 ${isActive? "bg-mocha-dark text-eggshell": `${isMouseOver ? "bg-mocha-dark text-eggshell" : "bg-cream-dark"}`}  transition-colors`} onClick={() => setIsActive(() => !isActive)} onMouseEnter={() => setIsMouseOver(true)} onMouseLeave={() => setIsMouseOver(false)}>
+        <div className="accordion my-4 mx-6 font-header text-3xl text-center cursor-pointer" id={props.bannerId}>
+            <div className={`accordion__banner py-4 ${isActive? "bg-mocha-dark text-eggshell": `${isMouseOver ? "bg-mocha-dark text-eggshell" : "bg-cream-dark"}`}  transition-colors`} onClick={() => setIsActive(() => !isActive)} onMouseEnter={() => setIsMouseOver(true)} onMouseLeave={() => setIsMouseOver(false)}>
                 {props.chapterName}
             </div>
-            <div className="flex flex-wrap justify-center mx-auto">
-                {isActive ? pagesDisplay : null}
+            <div className="accordion__content">
+                <div className="flex flex-wrap justify-center mx-auto">
+                    {pagesDisplay}
+                </div>
             </div>
-
         </div>
     )
 }
