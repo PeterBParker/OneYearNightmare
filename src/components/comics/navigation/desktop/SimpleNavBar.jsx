d0fcf440f22565e1a5f501b90cd2ce97be22c709
Nathan Harris <harrihaven2@gmail.com>
2023-03-22 11:33:27 -0500

    added main nav bar interactions and spacing

--- src/components/comics/navigation/desktop/SimpleNavBar.jsx
+++ src/components/comics/navigation/desktop/SimpleNavBar.jsx
@@ -1,9 +1,9 @@
-import NavBarLinks from './NavBarLinks';
+import NavBarLinks from "./NavBarLinks";
 
 export default function SimpleNavBar(props) {
-    return(
-        <div className="mt-4 mb-6 mr-auto flex flex-row justify-center text-2xl text-grey-light">
-            <NavBarLinks {...props}/>
-        </div>
-    )
+  return (
+    <div className="navbar-width transition-colors mt-4 mb-6 mr-auto flex flex-row justify-between mr-auto ml-auto text-2xl text-grey-light">
+      <NavBarLinks {...props} />
+    </div>
+  );
 }
