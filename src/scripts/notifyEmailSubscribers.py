72c565b90ccf33206816b103c0f46715c90fdc30
Nathan Harris <harrihaven2@gmail.com>
2023-09-09 20:04:02 -0500

    added schedule link and pages

--- src/scripts/notifyEmailSubscribers.py
+++ src/scripts/notifyEmailSubscribers.py
@@ -11,39 +11,43 @@ from time import sleep
 
 class EmailNotifier:
     """Handles notifying users and completing the email html template"""
+
     def __init__(self, from_email, new_page_link, contact_list_id, unsub_group):
         self.list_id = contact_list_id
         self.new_page_link = new_page_link
         self.sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
         self.from_email = from_email
         self.unsub_group = unsub_group
-    
+
     def get_email_export_urls(self, pending_export_id):
         """Handles requesting an export and returning a list of the ready-to-download url files"""
         print("Waiting for email contacts to export...")
         export_pending = True
         download_urls = None
+
         def handle_pending():
             sleep(5)
         while export_pending:
-                # check status
-                response = self.sg.client.marketing.contacts.exports._(pending_export_id).get()
-                if response.status_code != 200:
-                    print(f"Error: Encountered {response.status_code} error in checking that status of the export.")
-                    print(response.body)
+            # check status
+            response = self.sg.client.marketing.contacts.exports._(
+                pending_export_id).get()
+            if response.status_code != 200:
+                print(
+                    f"Error: Encountered {response.status_code} error in checking that status of the export.")
+                print(response.body)
+                handle_pending()
+            else:
+                json_response = json.loads(response.body)
+                status = json_response["status"]
+                if status == "pending":
                     handle_pending()
-                else:
-                    json_response = json.loads(response.body)
-                    status = json_response["status"]
-                    if status == "pending":
-                        handle_pending()
-                    elif status == "failed":
-                        print(json_response["message"])
-                        print("Contacts export failed.")
-                        export_pending = False
-                    elif status == "ready":
-                        download_urls = json_response["urls"]
-                        export_pending = False
+                elif status == "failed":
+                    print(json_response["message"])
+                    print("Contacts export failed.")
+                    export_pending = False
+                elif status == "ready":
+                    download_urls = json_response["urls"]
+                    export_pending = False
         return download_urls
 
     def download_and_extract_emails(self, download_urls):
@@ -60,7 +64,8 @@ class EmailNotifier:
     def subscriber_emails(self):
         """Provides the complete list of subscribers"""
         try:
-            response = self.sg.client.marketing.contacts.exports.post(request_body={"list_ids":["887040bc-71a4-489a-874a-c4cc0391f890"], "file_type":"csv"})
+            response = self.sg.client.marketing.contacts.exports.post(
+                request_body={"list_ids": ["887040bc-71a4-489a-874a-c4cc0391f890"], "file_type": "csv"})
             if response.status_code != 202:
                 print(response.status_code)
                 print(response.body)
@@ -96,10 +101,10 @@ class EmailNotifier:
         if self.new_page_link:
             message.dynamic_template_data = {"readUrl": self.new_page_link}
         return message
-    
+
     def notify_subscribers(self):
         """The main function of this class. Builds and sends the template email."""
-        to_emails = map(To, self.subscriber_emails())
+        to_emails = self.subscriber_emails()
         message = self.make_template_message(to_emails)
         response = self.sg.send(message=message)
         if response.status_code == 202:
